b33ec53e4df11b918c4ff7d62b001e861fcff5d5
Komal <divyavairagade@gmail.com>
2022-04-11 23:55:53 +0530

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+MyFirstConsoleApp.UC1.EmployeeAttendance();
+MyFirstConsoleApp.UC2.EmployeeWagesCal();
+MyFirstConsoleApp.UC3.PartTimeEmpWages();
+MyFirstConsoleApp.UC4.PartTimeEmpWages();
+MyFirstConsoleApp.UC5.totalEmpWage();
+MyFirstConsoleApp.UC6.totalEmpWage();
+MyFirstConsoleApp.UC7.computeEmpWage();
